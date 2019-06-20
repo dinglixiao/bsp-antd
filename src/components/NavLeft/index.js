eef7f2de207c700604e4174e02a7d137235d9e96
Administrator <1713716789@qq.com>
2019-06-21 00:11:56 +0800

    file changes

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,12 +1,27 @@
 import React, {Component} from "react";
+import {Menu} from "antd";
+
 import './index.less'
+import logo from "./../../logo.svg"
+import SubMenu from "antd/lib/menu/SubMenu";
 
 class NavLeft extends Component {
 
     render() {
         return (
             <div>
-                NavLeft
+                <div className="logo">
+                    <img src={logo} alt=""/>
+                    <h1>Imooc MS</h1>
+                </div>
+                <Menu theme={"dark"}>
+                    <SubMenu key="sub1">
+                        <Menu.Item key="9">Option 9</Menu.Item>
+                        <Menu.Item key="10">Option 10</Menu.Item>
+                        <Menu.Item key="11">Option 11</Menu.Item>
+                        <Menu.Item key="12">Option 12</Menu.Item>
+                    </SubMenu>
+                </Menu>
             </div>
         )
     }
--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {Component} from "react";
 import {Col, Row} from 'antd';
-import './index.less';
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import NavLeft from "../../components/NavLeft";
 
-function Admin() {
-    return (
-        <Row>
-            <Col span={3}>
-                <NavLeft></NavLeft>
-            </Col>
-            <Col span={21}>
-                <Header></Header>
-                <Row></Row>
-                <Footer></Footer>
-            </Col>
-        </Row>
-    );
+import './index.less';
+
+class Admin extends Component {
+
+    render() {
+        return (
+            <Row className="container">
+                <Col className="nav-left" span={3}>
+                    <NavLeft></NavLeft>
+                </Col>
+                <Col className="main" span={21}>
+                    <Header></Header>
+                    <Row className="content">
+
+                    </Row>
+                    <Footer></Footer>
+                </Col>
+            </Row>
+        )
+    }
 }
 
-export default Admin;
+export default Admin
