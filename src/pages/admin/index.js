6fce1a38cfdde66d4c80b5f7dc2738da7c316c7a
dinglixiao <1713716789@qq.com>
2019-06-20 17:59:41 +0800

    file changes

---
+++ src/pages/admin/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'antd';
+import './index.less';
+
+function Admin() {
+    return (
+        <div className="Admin">
+            <Button type="primary">Button</Button>
+        </div>
+    );
+}
+
+export default Admin;
