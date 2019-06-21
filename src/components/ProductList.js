6a035806e425bad2f7a169c553fd9e8e31d6e5f5
dinglixiao <1713716789@qq.com>
2019-06-21 14:33:07 +0800

    file changes

---
+++ src/components/ProductList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Button, Popconfirm, Table} from 'antd';
+
+const ProductList = ({onDelete, products}) => {
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  }, {
+    title: 'Actions',
+    render: (text, record) => {
+      return (
+          <Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
+            <Button>Delete</Button>
+          </Popconfirm>
+      );
+    },
+  }];
+  return (
+      <Table
+          dataSource={products}
+          columns={columns}
+      />
+  );
+};
+
+ProductList.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  products: PropTypes.array.isRequired,
+};
+
+export default ProductList;
