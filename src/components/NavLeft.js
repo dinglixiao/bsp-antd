505bbc1c64ac6e5a14f4f046daf4fa3a623d512c
dinglixiao <1713716789@qq.com>
2019-06-21 17:18:14 +0800

    file changes

--- src/components/NavLeft.js
+++ src/components/NavLeft.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import "./../styles/nav-left.less"
+import logo from "./../assets/logo.svg"
+import {Menu} from "antd";
+import SubMenu from "antd/lib/menu/SubMenu";
+
 const NavLeft = () => {
 
   return (
       <div>
-        NavLeft
+        <div className="logo">
+          <img src={logo} alt=""/>
+          <h1>管理平台</h1>
+        </div>
+        <Menu theme={"dark"} style={{height: "calc(100vh)"}}>
+          <Menu.Item>菜单项</Menu.Item>
+          <SubMenu title="子菜单">
+            <Menu.Item>子菜单项</Menu.Item>
+          </SubMenu>
+        </Menu>
       </div>
   );
 };
